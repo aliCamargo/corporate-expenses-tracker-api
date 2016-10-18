19d9f6216198eb7afd78547d5d5752840beddf13
Ali Camargo <ali.camargo@hotmail.com>
2016-10-17 20:46:20 -0500

    Fix CORS

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,23 @@ module CorporateExpressTracker
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
     end
 
+    config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
+      allow do
+        origins '*'
+
+        resource '/cors',
+                 :headers => :any,
+                 :methods => [:post],
+                 :credentials => true,
+                 :max_age => 0
+
+        resource '*',
+                 :headers => :any,
+                 :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+                 :max_age => 0
+      end
+    end
+
     #-- Autoload lib path
     config.autoload_paths += %W(\#{config.root}/lib)
 
