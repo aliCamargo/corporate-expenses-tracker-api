8612412bbfabc116d8b6066d0387dbb96be788fe
Ali Camargo <ali.camargo@hotmail.com>
2016-10-18 20:09:42 -0500

    Add new validation on user model and add depend destroy trips

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :rememberable, :trackable, :validatable
 
-  has_many :trips
+  has_many :trips, dependent: :destroy
 
   enum role: { admin: 0, employee: 1 }
   enum gender: { male: 0, female: 1 }
 
   validates :access_token, uniqueness: true, unless: Proc.new {|u| u.access_token.blank? }
+  validates :role, :first_name, :last_name, presence: true
 
   def generate_access_token!
     begin
       self.access_token = Devise.friendly_token
     end while self.class.exists?( access_token: access_token )
+  end
+
+  def short_name
+    "#{self.first_name.first.upcase}. #{self.last_name.downcase.titleize}"
   end
 end
